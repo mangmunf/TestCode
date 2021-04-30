b070091a45048aef38299572c426aa5dea56c21c
mangmun <33015334+mangmunf@users.noreply.github.com>
2021-04-30 11:06:37 +0700

    Add files via upload

---
+++ Main.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Main.aspx.cs" Inherits="Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">LinkButton</asp:LinkButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Main.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Main : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!Page.IsPostBack)
+        {
+            TextBox1.Text = "Press Type Message";
+            TextBox1.Focus();
+
+            LinkButton1.Text = "Click !!";
+        }
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        Response.Write("<script>alert('" + TextBox1.Text + " ... Message !!');</script>");
+    }
+}
